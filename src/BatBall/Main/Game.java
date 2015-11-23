c1b061b88d42813fee06618309e399bad235d17e
Philip <PhilipColman@users.noreply.github.com>
2015-11-23 14:32:37 +0000

    Added main window, game loop and update and render methods.

--- src/BatBall/Main/Game.java
+++ src/BatBall/Main/Game.java
@@ -20,6 +20,96 @@
 
 package BatBall.Main;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
+public class Game extends Canvas implements Runnable{
+
+    private boolean running = false;
+    private int frames = 0;
+    private int fps, ups;
+    private int updates;
+
+    private Window window;
+    private Thread thread;
+
+    public Game() {
+        window = new Window(this);
+        start();
+    }
+
+    public static void main(String[] args){
+        new Game();
+    }
+
+    @Override
+    public void run() {
+        this.requestFocus();
+
+        long lastTime = System.nanoTime();
+        double amountOfUpdates = 60.0;
+        double ns = 1000000000 / amountOfUpdates;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while (delta >= 1) {
+                update();
+                delta--;
+                updates++;
+            }
+            if(running){
+                render();
+                frames++;
+            }
+            if(System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                fps = frames;
+                frames = 0;
+                ups = updates;
+                updates = 0;
+                window.setTitle(String.format("Bat and Ball   |   UPS: %1$d  FPS: %2$d", ups, fps));
+            }
+        }
+        stop();
+    }
+
+    private void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, Window.getWindowWidth(), Window.getWindowWidth());
+
+        g.dispose();
+        bs.show();
+    }
+
+    private void update() {
+
+    }
+
+    private synchronized void start(){
+        running = true;
+        this.thread = new Thread(this, "Main Thread");
+        thread.start();
+    }
+
+    private synchronized void stop(){
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/BatBall/Main/Window.java
@@ -0,0 +1,49 @@
+/**
+ * Created by philip on 23/11/15.
+ * <p/>
+ * BatBall is a basic bat ball game.
+ * Copyright (C) 2015  philip
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package BatBall.Main;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.Toolkit;
+
+public class Window extends JFrame{
+
+    private static Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    public Window(Game game){
+        super("Bat and Ball");
+        //this.setUndecorated(true);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.add(game);
+        this.setVisible(true);
+    }
+
+    public static int getWindowWidth() {
+        return (int) toolkit.getScreenSize().getWidth();
+    }
+
+    public static int getWindowHeight() {
+        return (int)toolkit.getScreenSize().getHeight();
+    }
+}
